5ac8b961024644d60dd8dc50e42f9edddfccb515
ValentinBrest <fixator03@yandex.ru>
2021-03-08 23:00:47 +0300

    feat: add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function reverse (n) {
-  
+  let a =  n.toString();
+  let arr = [];
+if (n >= 0){
+    for (let i = 0; i < a.length; i++) {
+     arr[i] = a.charAt(i);
+   }
+     arr.reverse();
+       return (Number(arr.join('')));
+} else {
+  n = n * (-1);
+  a =  n.toString();
+  for (let i = 0; i < a.length; i++) {
+    arr[i] = a.charAt(i);
+  }
+    arr.reverse();
+     return (Number(arr.join('')) );
 }
+}
+  
